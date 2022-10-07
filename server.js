beb6d083fb4c420018d992e723a682e6a9b73cf6
varun <you@example.com>
2022-10-07 17:13:02 -0400

    initial commit

--- server.js
+++ server.js
@@ -1,3 +1,15 @@
+/*************************************************************************
+* BTI325– Assignment 2
+* I declare that this assignment is my own work in accordance with Seneca Academic
+Policy. No part * of this assignment has been copied manually or electronically from any
+other source
+* (including 3rd party web sites) or distributed to other students.
+*
+* Name: Varun Kakkar Student ID: 124524216 Date: 7/10/2022
+*
+* Your app’s URL (from Cyclic) : https://charming-garb-clam.cyclic.app/
+*
+*************************************************************************/
 var express = require("express");
 var app = express();
 var data = require("./data-service.js")
